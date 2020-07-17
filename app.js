39609fa24baca72627bd29707890bc85d59a21a8
azlan <mhdazlanabaziz@gmai.com>
2020-07-17 16:25:21 +0800

    basic / routing

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+
+const indexRouter = require('./routes/index')
+
+
+app.use('/', indexRouter)
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`))

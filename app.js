57e33fa522caf441a170e289c3afea9fe8a4d945
azlan <mhdazlanabaziz@gmai.com>
2020-07-17 16:31:52 +0800

    finish basic user routes

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 const indexRouter = require('./routes/index')
+const userRouter = require('./routes/users')
 
 
 app.use('/', indexRouter)
+app.use('/user', userRouter)
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
